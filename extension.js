b9cee4c3abfbb2fd647610976028c393f13ba360
Ismaïl NGUYEN <ismail.nguyen@loccitane.com>
2021-11-16 18:25:13 +0100

    Fix folder herarchy issue

--- extension.js
+++ extension.js
@@ -1,6 +1,4 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const app = require('./app.js');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,20 +7,11 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "fortuneteller" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('fortuneteller.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from fortuneteller!');
-	});
+	
+	let disposable = app.dispose();
 
 	context.subscriptions.push(disposable);
 }
